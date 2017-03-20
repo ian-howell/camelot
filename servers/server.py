c3385cfe41fc1297bb3db2418b47e867597e0850
Ian <ian.howell0@gmail.com>
2017-03-19 19:40:02 -0500

    Initial attempt at a server

---
+++ servers/server.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+import select
+import socket
+import socketserver
+import threading
+import time
+
+SOCKET_LIST = []
+
+def main():
+    server = ThreadedTCPServer(("0.0.0.0", 9009), ThreadedTCPRequestHandler)
+    ip, port = server.server_address
+    server.socket.listen(10)
+
+    server_thread = threading.Thread(target=server.serve_forever)
+    server_thread.dameon = True
+    server_thread.start()
+    print("Server loop running in thread:", server_thread.name)
+
+    try:
+        # Loop forever
+        while True:
+            pass
+    except KeyboardInterrupt:
+        print("Cleaning up server....")
+        server.shutdown()
+        server.server_close()
+        print("Done! Goodbye")
+
+
+## ThreadedTCPRequestHandler
+#
+#  A TCP Request handler to be used by a TCP server
+class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
+
+    ## handle
+    #
+    #  Recieve and manage incoming requests
+    #  @param self The object pointer
+    def handle(self):
+        # Add the new client socket to the socket list
+        SOCKET_LIST.append(self.request)
+
+        while True:
+            # Prompt the OS for all of the sockets which are ready to read from
+            ready_to_read, ignore1, ignore2 = select.select(SOCKET_LIST,[],[],0)
+            for sock in ready_to_read:
+                # Receive the data from that socket
+                data = str(self.request.recv(1024), 'ascii')
+                print("Received {}".format(data))
+                # Get this client's thread
+                cur_thread = threading.current_thread()
+                for s in SOCKET_LIST:
+                    # If this is someone else's client
+                    if s != sock:
+                        response = bytes("({}): {}".format(cur_thread.name, data), 'ascii')
+                    else:
+                        response = bytes("(you): {}".format(data), 'ascii')
+                    s.sendall(response)
+
+
+## ThreadedTCPServer
+#
+#  A TCP server for managing connections
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    def server_bind(self):
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.bind(self.server_address)
+
+
+if __name__ == "__main__":
+    main()
