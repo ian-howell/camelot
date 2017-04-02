85c14f25b088b7bcad2e0c709bc7ace1e752a392
Zachary Wileman <zdw27f@mst.edu>
2017-04-02 05:31:31 -0500

    [UPDATE]
    - Put server functions into their own class
    - Further simplifications

--- database/server_example.py
+++ database/server_example.py
@@ -1,4 +1,5 @@
 from database import Camelot_Database
+from server import Camelot_Server
 import json
 
 
@@ -85,162 +86,20 @@ def test_client_change_password():
         }
     })
 
-def change_password(mydb, client_request):
-    try:
-        username = client_request['change_password']['username']
-        current_password = client_request['change_password']['current_password']
-        new_password = client_request['change_password']['new_password']
-    except KeyError:
-        return json.dumps({
-            "error": "The JSON file sent didn't contain valid information."
-        }, indent=4)
-
-    return mydb.change_password(username, current_password, new_password)
-
-def leave_channel(mydb, client_request):
-    channel_name = client_request['leave_channel']
-
-    # Eventaully, when the server is working, you'll grab the user making the request
-    # and set it to the admin. A check will also need to be done to make sure that
-    # a session has a user
-    #user = session.user
-    user = "zach" #Temporary
-
-    return mydb.leave_channel(channel_name, user)
-
-def get_users_in_channel(mydb, client_request):
-    try:
-        channel_name = client_request['get_users_in_channel']
-    except KeyError:
-        return json.dumps({
-            "error": "The JSON file sent didn't contain valid information."
-        }, indent=4)
-
-    return mydb.get_users_in_channel(channel_name)
-
-def delete_account(mydb, client_request):
-    # Check that no one is logged in under the given username
-    # Checks what channels the user is an admin of because those channels
-    # will be deleted; these channels need to be known so that users can
-    # be notified of channels that are deleted.
-
-    try:
-        username = client_request['delete_account']['username']
-        password = client_request['delete_account']['password']
-    except KeyError:
-        return json.dumps({
-            "error": "The JSON file sent didn't contain valid information."
-        }, indent=4)
-
-    return mydb.delete_account(username, password)
-
-def delete_channel(mydb, client_request):
-    channel_name = client_request['delete_channel']
-
-    # Eventaully, when the server is working, you'll grab the user making the request
-    # and set it to the admin. A check will also need to be done to make sure that
-    # a session has a user
-    #user = session.user
-    user = "username" #Temporary
-
-    return mydb.delete_channel(channel_name, user)
-
-def create_channel(mydb, client_request):
-    channel_name = client_request['create_channel']
-
-    # Eventaully, when the server is working, you'll grab the user making the request
-    # and set it to the admin. A check will also need to be done to make sure that
-    # a session has a user
-    #admin = session.user
-    admin = "username" #Temporary
-
-    return mydb.create_channel(channel_name, admin)
-
-# NOTE: Some variable will also need to be passed to identify which
-#       user is trying to join a channel. I'm guessing this variable
-#       will be created when the client logs into the server.
-def join_channel(mydb, client_request):
-
-    # For now, I'll assume with a local variable the client's username
-    # Eventaully, this will come from session.user
-    username = 'zach'
-
-    # Makes sure there are channels for the user to join
-    current_channels_available = mydb.get_channels()
-    if 'error' in current_channels_available.keys():
-        return current_channels_available
-
-    channels_user_wants_to_join = [channel for channel in client_request['join_channel']]
-
-    # Make sure the user isn't trying to join invalid channels
-    for channel in channels_user_wants_to_join:
-        if channel not in current_channels_available['channels']:
-            return json.dumps({
-                "error": "The user is trying to join a channel that doesn't exist."
-            }, indent=4)
-
-    # Connects the user to the specified channels and stores the information in the database
-    mydb.add_channels_to_user_info(username, channels_user_wants_to_join)
-
-def login(mydb, client_request):
-    # Makes sure the user is sending valid information
-    if client_request['login']['username'] and client_request['login']['password']:
-        client_username = client_request['login']['username']
-        client_password = client_request['login']['password']
-
-        result = mydb.check_username_password_in_database(client_username, client_password)
-        if result:
-            return result
-        else:
-            return mydb.get_channels()
-
-    else:
-        return json.dumps({
-            "error": "The JSON file sent didn't contain valid information."
-        }, indent=4)
-
-def create_account(mydb, client_request):
-    if client_request['create_account']['username'] and client_request['create_account']['password']:
-        client_username = client_request['create_account']['username']
-        client_password = client_request['create_account']['password']
-
-        result = mydb.create_account(client_username, client_password)
-        if result:
-            return result
-    else:
-        return json.dumps({
-            "error": "The JSON file sent didn't contain valid information."
-        }, indent=4)
-
-def new_message(mydb, client_request):
-    return json.dumps(client_request, indent=4)
-
 def send_to_client(response):
     print("This is what will get sent to the client:\n")
     print(response)
     print('')
 
 if __name__ == '__main__':
+    server = Camelot_Server()
     mydb = Camelot_Database()
     mydb.create_tables('tables.sql')
     #mydb.insert_data('data.sql')
 
-    server_functions = {
-        'create_account':create_account,
-        'login':login,
-        'join_channel':join_channel,
-        'new_message':new_message,
-        'create_channel':create_channel,
-        'delete_channel':delete_channel,
-        'delete_account':delete_account,
-        'get_users_in_channel':get_users_in_channel,
-        'leave_channel':leave_channel,
-        'change_password':change_password
-    }
-
-    client_request = json.loads(test_client_bad_json())
+    #client_request = json.loads(test_client_bad_json())
     #client_request = json.loads(test_client_create_account())
-    #client_request = json.loads(test_client_login())
+    client_request = json.loads(test_client_login())
     #client_request = json.loads(test_client_join_channel())
     #client_request = json.loads(test_client_new_message())
     #client_request = json.loads(test_client_create_channel())
@@ -252,8 +111,8 @@ if __name__ == '__main__':
 
     for operation in client_request.keys():
         try:
-            response = server_functions[operation](mydb, client_request)
-        except KeyError:
+            response = getattr(server, operation)(mydb, client_request)
+        except AttributeError:
             response = json.dumps({
                 "error": "The JSON file sent didn't contain valid information."
             }, indent=4)
