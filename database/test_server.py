39cc006e6434fb3ddffb17a546678e555b825791
Zachary Wileman <zdw27f@mst.edu>
2017-04-02 15:06:51 -0500

    [UPDATE] Added tests for login function

--- database/test_server.py
+++ database/test_server.py
@@ -87,3 +87,102 @@ def test_create_account_username_already_taken():
 
     assert expected_response == result
     mydb.empty_tables()
+
+def test_create_account_success():
+    server = Camelot_Server()
+    mydb = Camelot_Database()
+
+    client_request = json.loads(json.dumps({
+        "create_account": {
+            "username": "username",
+            "password": "password",
+        }
+    }, indent=4))
+
+    expected_response = None
+
+    result = server.create_account(mydb, client_request)
+
+    assert expected_response == result
+    mydb.empty_tables()
+
+def test_login_invalid_json():
+    server = Camelot_Server()
+    mydb = Camelot_Database()
+
+    client_request = json.loads(json.dumps({
+        "login": {
+            "username_invalid": "username",
+            "password": "password",
+        }
+    }, indent=4))
+
+    expected_response = json.dumps({
+        "error": "The JSON file sent didn't contain valid information."
+    }, indent=4)
+
+    result = server.login(mydb, client_request)
+
+    assert expected_response == result
+    mydb.empty_tables()
+
+def test_login_user_password_combination_not_in_database():
+    server = Camelot_Server()
+    mydb = Camelot_Database()
+
+    client_request = json.loads(json.dumps({
+        "login": {
+            "username": "username",
+            "password": "password",
+        }
+    }, indent=4))
+
+    expected_response = json.dumps({
+        "error": "The username/password combination do not exist in the database."
+    }, indent=4)
+
+    result = server.login(mydb, client_request)
+
+    assert expected_response == result
+    mydb.empty_tables()
+
+def test_login_no_channels_available():
+    server = Camelot_Server()
+    mydb = Camelot_Database()
+
+    client_request = json.loads(json.dumps({
+        "login": {
+            "username": "username",
+            "password": "password",
+        }
+    }, indent=4))
+
+    expected_response = json.dumps({
+        "error": "No channels exist in the database."
+    }, indent=4)
+
+    mydb.create_account("username", "password")
+    result = server.login(mydb, client_request)
+
+    assert expected_response == result
+    mydb.empty_tables()
+
+def test_login_success():
+    server = Camelot_Server()
+    mydb = Camelot_Database()
+
+    client_request = json.loads(json.dumps({
+        "login": {
+            "username": "username",
+            "password": "password",
+        }
+    }, indent=4))
+
+    mydb.create_account("username", "password")
+    mydb.create_channel("ChannelTest", "username")
+
+    expected_response = mydb.get_channels()
+    result = server.login(mydb, client_request)
+
+    assert expected_response == result
+    mydb.empty_tables()
